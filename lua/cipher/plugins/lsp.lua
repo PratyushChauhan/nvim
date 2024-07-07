89552286c6f53c60e55f1f27e358b5fd47f28e05
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2024-07-07 23:00:22 +0530

    feat add dart ls, better remaps

--- lua/cipher/plugins/lsp.lua
+++ lua/cipher/plugins/lsp.lua
@@ -1,100 +1,153 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
+    "neovim/nvim-lspconfig",
     dependencies = {
-        -- LSP Support
-        { 'neovim/nvim-lspconfig' },             -- Required
-        { 'williamboman/mason.nvim' },           -- Optional:
-        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-        -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },         -- Required
-        { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-        { 'hrsh7th/cmp-buffer' },       -- Optional
-        { 'hrsh7th/cmp-path' },         -- Optional
-        { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-        { 'hrsh7th/cmp-nvim-lua' },     -- Optional
-
-        -- Snippets
-        { 'L3MON4D3/LuaSnip' },             -- Required
-        { 'rafamadriz/friendly-snippets' }, -- Optional
-        { 'j-hui/fidget.nvim' },
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
     },
     config = function()
-        local lsp = require("lsp-zero")
-        local lsp_config = require("lspconfig")
-        lsp.preset("recommended")
-
-        lsp.ensure_installed({
-            'tsserver',
-            'rust_analyzer',
-            'lua_ls',
-        })
-
-        -- Fix Undefined global 'vim'
-        lsp.nvim_workspace()
-
-
         local cmp = require('cmp')
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        local cmp_keymaps = lsp.defaults.cmp_mappings({
-            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-        })
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
 
-        cmp_keymaps['<Tab>'] = nil
-        cmp_keymaps['<S-Tab>'] = nil
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls",
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
+                    print(server_name)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+                --[[
+                ["dartls"] = function()
+                    local lspconfig = require("lspconfig")
+                    require 'lspconfig'.dartls.setup {
+                        root_dir = lspconfig.util.root_pattern("pubspec.yaml"),
+                        cmd = { "dart", "language-server", "--protocol=lsp" },
+                        capabilities = capabilities,
+                        init_options = {
+                            closingLabels = true,
+                            flutterOutline = true,
+                            onlyAnalyzeProjectsWithOpenFiles = true,
+                            outline = true,
+                            suggestFromUnimportedLibraries = true
+                        },
+                        filetypes = { "dart" },
+                        settings = {
+                            dart = {
+                                completeFunctionCalls = true,
+                                showTodos = true
+                            }
+                        }
+                    }
+                end,
+                --]]
 
-        lsp.setup_nvim_cmp({
-            mapping = cmp_keymaps
-        })
-
-        lsp.set_preferences({
-            suggest_lsp_servers = true,
-            sign_icons = {
-                error = 'E',
-                warn = 'W',
-                hint = 'H',
-                info = 'I'
+                zls = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.zls.setup({
+                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                        settings = {
+                            zls = {
+                                enable_inlay_hints = true,
+                                enable_snippets = true,
+                                warn_style = true,
+                            },
+                        },
+                    })
+                    vim.g.zig_fmt_parse_errors = 0
+                    vim.g.zig_fmt_autosave = 0
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
             }
         })
-        local onAttach = function(client, bufnr)
-            local opts = { buffer = bufnr, remap = false }
 
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end
-
-        lsp.on_attach(onAttach)
-        lsp_config["dartls"].setup({
-
-            on_attach = onAttach,
+        local lspconfig = require("lspconfig")
+        require 'lspconfig'.dartls.setup {
+            root_dir = lspconfig.util.root_pattern("pubspec.yaml"),
+            cmd = { "dart", "language-server", "--protocol=lsp" },
+            capabilities = capabilities,
+            init_options = {
+                closingLabels = true,
+                flutterOutline = true,
+                onlyAnalyzeProjectsWithOpenFiles = true,
+                outline = true,
+                suggestFromUnimportedLibraries = true
+            },
+            filetypes = { "dart" },
             settings = {
                 dart = {
-                    analysisExcludedFolder = {
-                        vim.fn.expand("$Home/AppData/Local/Pub/Cache"),
-                        vim.fn.expand("$Home/.pub-cache"),
-                        vim.fn.expand("C:/dev/tooling/flutter"),
-                    }
+                    completeFunctionCalls = true,
+                    showTodos = true
                 }
-
             }
+        }
 
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ['<Tab>'] = nil,
+                ['<S-Tab>'] = nil,
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
+            })
         })
-        lsp.setup()
-        require("fidget").setup({})
+
         vim.diagnostic.config({
-            virtual_text = true
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
         })
     end
 }
