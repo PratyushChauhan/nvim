227c82cd31376ca1cabd2ccdcd4354e6fcb637cf
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2024-07-07 19:02:19 +0530

    add tokyo night color

--- lua/cipher/plugins/colors.lua
+++ lua/cipher/plugins/colors.lua
@@ -1,16 +1,44 @@
-return{
+return {
+    {
         'rose-pine/neovim',
         name = 'rose-pine',
+        priority = 1000,
+        lazy = false,
         config = function()
             vim.cmd('colorscheme rose-pine')
-         function ColorMyPencils(color)
+            function ColorMyPencils(color)
                 print("color my pencil")
                 color = color or "rose-pine"
                 vim.cmd.colorscheme(color)
 
-                vim.api.nvim_set_hl(0,"Normal",{bg = "None" })
-                vim.api.nvim_set_hl(0,"NormalFloat",{bg = "None" })
+                vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
+                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None" })
             end
+
             ColorMyPencils()
         end
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 100,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the defal
+                style = "storm", -- The theme comes :
+                transparent = true, -- Enable this to
+                terminal_colors = true, -- Configure
+                styles = {
+                    -- Style to be applied to differ
+                    -- Value is any valid attr-list
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    sidebars = "dark", -- style for
+                    floats = "dark", -- style for flo
+                }
+            })
+        end
+    }
 }
