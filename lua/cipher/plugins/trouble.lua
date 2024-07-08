587a0d9c03a08c8551a3e947c3cc980033791498
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2024-07-08 13:09:58 +0530

    fix trouble

--- lua/cipher/plugins/trouble.lua
+++ lua/cipher/plugins/trouble.lua
@@ -2,20 +2,16 @@ return {
     {
         "folke/trouble.nvim",
         config = function()
-            require("trouble").setup({
-                icons = false,
-            })
-
             vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle({})
+                require("trouble").toggle({ mode = "diagnostics" })
             end)
 
             vim.keymap.set("n", "[t", function()
-                require("trouble").next({ skip_groups = true, jump = true });
+                require("trouble").next({ skip_groups = true, jump = true })
             end)
 
             vim.keymap.set("n", "]t", function()
-                require("trouble").previous({ skip_groups = true, jump = true });
+                require("trouble").prev({ skip_groups = true, jump = true })
             end)
         end
     },
