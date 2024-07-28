0796d39b83fe3bdfec472f16832d0f6362fbed68
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2024-07-28 18:50:40 +0530

    edit bindings for copilot

--- lua/cipher/plugins/copilot.lua
+++ lua/cipher/plugins/copilot.lua
@@ -21,17 +21,17 @@ return {
                     },
                 },
                 suggestion = {
-                    enabled = false,
+                    enabled = true,
                     auto_trigger = true,
                     hide_during_completion = true,
                     debounce = 75,
                     keymap = {
-                        accept = "<M-l>",
+                        accept = "<Tab>",
                         accept_word = false,
                         accept_line = false,
                         next = "<M-]>",
                         prev = "<M-[>",
-                        dismiss = "<C-]>",
+                        dismiss = "<BS>",
                     },
                 },
                 filetypes = {
